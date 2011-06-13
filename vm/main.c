90b59615ed6e6f4cc57f17198e0a17a292acac5d
jsz <jszpilewski@me.com>
2011-06-13 21:32:20 +0200

    cosmetics baby

--- vm/main.c
+++ vm/main.c
@@ -5,17 +5,17 @@ int main(int argc, char **argv)
 {
 	cpu_t *mycpu = cpu_new();
 
-	uint8_t opcodes[] = {0xa9, 0xaa,
+	uint8_t program[] = {0xa9, 0x00,
 	                     0xa2, 0x1f,
 	                     0x9d, 0x00, 0x30,
 	                     0xbd, 0x00, 0x30,
 	                     0x69, 0x01,
 	                     0x9d, 0x00, 0x30,
-											 0xff,
+	                     0xff,
 	                     0x4c, 0x07, 0x20,
 	                    };
 
-	cpu_exec(mycpu, 0x2000, opcodes, sizeof(opcodes));
+	cpu_exec(mycpu, 0x2000, program, sizeof(program));
 
 	cpu_free(mycpu);
 
